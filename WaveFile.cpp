4d1518d95c87658c583dbea20984085848c60b94
Gabriel Klavans <gabeklav@gmail.com>
2021-10-31 01:34:54 -0400

    - Change types to something less explicit but more reasonable
    - Generate dummy file with valid header

--- WaveFile.cpp
+++ WaveFile.cpp
@@ -1,32 +1,31 @@
-#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
 struct SMinimalWaveFileHeader
 {
     //the main chunk
-    unsigned char m_szChunkID[4];
-    uint32_t m_nChunkSize;
-    unsigned char m_szFormat[4];
+    unsigned char m_szChunkID[4]; // "RIFF"
+    long m_nChunkSize;            // 4 + number of chunks
+    unsigned char m_szFormat[4];  // "WAVE"
 
-    //sub chunk 1 "fmt "
-    unsigned char m_szSubChunk1ID[4];
-    uint32_t m_nSubChunk1Size;
-    uint16_t m_nAudioFormat;
-    uint16_t m_nNumChannels;
-    uint32_t m_nSampleRate;
-    uint32_t m_nByteRate;
-    uint16_t m_nBlockAlign;
-    uint16_t m_nBitsPerSample;
+    //sub chunk 1
+    unsigned char m_szSubChunk1ID[4]; // "fmt "
+    long m_nSubChunk1Size;
+    short m_nAudioFormat; // != 1 ? compressed : uncompressed
+    unsigned short m_nNumChannels;
+    unsigned long m_nSampleRate;
+    unsigned long m_nByteRate;
+    unsigned short m_nBlockAlign;
+    unsigned short m_nBitsPerSample;
 
     //sub chunk 2 "data"
-    unsigned char m_szSubChunk2ID[4];
-    uint32_t m_nSubChunk2Size;
+    unsigned char m_szSubChunk2ID[4]; // "data"
+    long m_nSubChunk2Size;
 
     //then comes the data!
 };
 
-bool WriteWaveFile(const char *szFileName, void *pData, int32_t nDataSize, int16_t nNumChannels, int32_t nSampleRate, int32_t nBitsPerSample)
+bool WriteWaveFile(const char *szFileName, void *pData, long nDataSize, unsigned short nNumChannels, unsigned long nSampleRate, unsigned long nBitsPerSample)
 {
     //open the file if we can
     FILE *File = fopen(szFileName, "w+b");
@@ -64,5 +63,17 @@ bool WriteWaveFile(const char *szFileName, void *pData, int32_t nDataSize, int16
 
     //close the file and return success
     fclose(File);
+    return true;
+}
+
+int main()
+{
+    long sampleRate = 44100;
+    long numSeconds = 4;
+    long nChannels = 1;
+    long numSamples = sampleRate * numSeconds * nChannels;
+    long *pData = new long[numSamples];
+    WriteWaveFile("sound.wav", pData, numSamples * sizeof(pData[0]), nChannels, sampleRate, sizeof(pData[0]) * 8);
+
     return true;
 }
